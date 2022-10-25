ad7a9516d9ccee8e8f83c5cdc4e9dd0f679090c4
Anhelina <bogdanova_angelina@ukr.net>
2022-10-26 00:14:47 +0300

    connected qonsoll lib

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import './index.css'
+import '@qonsoll/react-design/dist/styles/styles.css'
+import '@qonsoll/react-design/dist/styles/vars/index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import App from './App'
+import { BrowserRouter } from 'react-router-dom'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
-);
+)
--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,28 +1,30 @@
-import React from 'react';
-import { UserAuth } from '../context/AuthContext';
+import { Button } from '@qonsoll/react-design'
+import React from 'react'
+import { UserAuth } from '../context/AuthContext'
 
 const Account = () => {
-  const { logOut, user } = UserAuth();
+  const { logOut, user } = UserAuth()
 
   const handleSignOut = async () => {
     try {
-      await logOut();
+      await logOut()
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 
   return (
-    <div className='w-[300px] m-auto'>
-      <h1 className='text-center text-2xl font-bold pt-12'>Account</h1>
+    <div className="w-[300px] m-auto">
+      <Button type="primary">hello</Button>
+      <h1 className="text-center text-2xl font-bold pt-12">Account</h1>
       <div>
         <p>Welcome, {user?.displayName}</p>
       </div>
-      <button onClick={handleSignOut} className='border py-2 px-5 mt-10'>
+      <button onClick={handleSignOut} className="border py-2 px-5 mt-10">
         Logout
       </button>
     </div>
-  );
-};
+  )
+}
 
-export default Account;
+export default Account
